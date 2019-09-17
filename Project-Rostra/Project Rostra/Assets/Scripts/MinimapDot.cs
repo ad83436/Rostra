4e086e6adefc278ee634d1a7596fa2e8f7862689
Dylan Tuck <45218496+Keli2k@users.noreply.github.com>
2019-09-16 20:13:37 -0400

    Add files via upload

---
+++ Project-Rostra/Project Rostra/Assets/Scripts/MinimapDot.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapDot : MonoBehaviour
+{
+    public float Speed;
+
+
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+    float horizontalMove;
+    float verticalMove;
+
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput.normalized * Speed;
+        horizontalMove = Input.GetAxisRaw("Horizontal") * Speed;
+        verticalMove = Input.GetAxisRaw("Vertical") * Speed;
+
+
+
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+}
