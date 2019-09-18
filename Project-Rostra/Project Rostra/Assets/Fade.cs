5ba67c4bb5d4560b91c808185de4bbae4fb0033b
MohdAAyyad <mohd.ayyad.maa@gmail.com>
2019-09-17 22:40:38 -0400

    Fading works + Added enemy stuff into the scene

---
+++ Project-Rostra/Project Rostra/Assets/Fade.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Fade : MonoBehaviour
+{
+    private Image thisImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisImage = gameObject.GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        thisImage.fillAmount -= 0.02f;
+        if(thisImage.fillAmount<=0.0f)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
--- Project-Rostra/Project Rostra/Assets/Scripts/Enemies/WMEnemy.cs
+++ Project-Rostra/Project Rostra/Assets/Scripts/Enemies/WMEnemy.cs
@@ -20,13 +20,17 @@ public class WMEnemy : MonoBehaviour
     {
         if(col.gameObject.tag.Equals("Player"))
         {
-            for (int i = 0; i < enemies.Length; i++)
-            {
-                enemySpwn.AddEnemyToSpawn(enemies[i], i, enemyLevels[i]);
-            }
-            SceneManager.LoadScene("Queue Scene", LoadSceneMode.Additive);
-            gameObject.SetActive(false);
-
+            TransitionIntoBattle();
         }
+    }
+
+    public void TransitionIntoBattle()
+    {
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            enemySpwn.AddEnemyToSpawn(enemies[i], i, enemyLevels[i]);
+        }
+        SceneManager.LoadScene("Queue Scene", LoadSceneMode.Additive);
+        gameObject.SetActive(false);
     }
 }
