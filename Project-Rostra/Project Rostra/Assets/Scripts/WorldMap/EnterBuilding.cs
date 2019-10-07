51c15a5dbe6c0104ecc32e0da0c54f6c57818575
Dylan Tuck <45218496+Keli2k@users.noreply.github.com>
2019-10-06 23:58:09 -0400

    Ayo i finally got this shit working so ya this is just the main locations and a dungeon enter script

---
+++ Project-Rostra/Project Rostra/Assets/Scripts/WorldMap/EnterBuilding.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class EnterBuilding : MonoBehaviour
+{//oo oo ah ah
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag.Equals("Player"))
+        {
+            Debug.Log("aksdjsa");
+            SceneManager.LoadScene("DungeonTest", LoadSceneMode.Additive);
+        }
+    }
+}
+
