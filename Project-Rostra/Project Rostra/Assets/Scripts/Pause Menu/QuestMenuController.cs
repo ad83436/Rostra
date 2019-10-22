e28ca7ee93ce547b4d44ece1118c33086dfcc6e5
Domara Shlimon <domara2000@gmail.com>
2019-10-22 19:36:56 -0400

    Settings menu now works

---
+++ Project-Rostra/Project Rostra/Assets/Scripts/Pause Menu/QuestMenuController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestMenuController : SubMenu {
+
+	#region Submenu Functions
+
+	public override void MenuUpdate() {
+		if (Cancel) {
+			ExitMenu();
+		}
+	}
+
+	#region Menu Events
+
+	public override void OnActive() { }
+	public override void OnInactive() { }
+	public override void OnInvisible() { }
+	public override void OnVisible() { }
+
+	#endregion
+
+	#endregion
+}
