b9a42310b9321ff3cc6264f51102db5e5db9be74
MohdAAyyad <mohd.ayyad.maa@gmail.com>
2019-10-22 00:54:50 -0400

    Corrected the playtest link to redirect the player toward the survey + Created a Game Manager to ensure the game runs on 60FPS with no VSync

---
+++ Project-Rostra/Project Rostra/Assets/Scripts/Levels/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private int targetFPS = 60;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = targetFPS;
+    }
+
+    private void Update()
+    {
+        if (Application.targetFrameRate != targetFPS)
+        {
+            Application.targetFrameRate = targetFPS;
+        }
+    }
+}
