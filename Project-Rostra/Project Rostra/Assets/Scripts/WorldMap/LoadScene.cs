ef0c40e9e65d809a875f892a35a388be82d6759c
SeanFowke <seanfowke@hotmail.com>
2019-11-02 19:51:58 -0400

    Good start on the Playtest 2

---
+++ Project-Rostra/Project Rostra/Assets/Scripts/WorldMap/LoadScene.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+	public string unloadScene;
+	public string sceneToLoad;
+	public bool unload;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.CompareTag("Player"))
+		{
+			SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Additive);
+			if (unload == true)
+			{
+				SceneManager.UnloadSceneAsync(unloadScene);
+			}
+
+		}
+	}
+}
