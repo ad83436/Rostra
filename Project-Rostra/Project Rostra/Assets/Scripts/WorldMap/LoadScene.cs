83d0085e7f231c0f0001f7289cf82d1692295b9b
SeanFowke <seanfowke@hotmail.com>
2019-11-05 22:04:54 -0500

    you can load between scenes and fixed the collision bug

--- Project-Rostra/Project Rostra/Assets/Scripts/WorldMap/LoadScene.cs
+++ Project-Rostra/Project Rostra/Assets/Scripts/WorldMap/LoadScene.cs
@@ -5,9 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour
 {
-	public string unloadScene;
 	public string sceneToLoad;
+	public string sceneToUnLoad;
 	public bool unload;
+	public Vector2 moveOut;
 	public static GameObject player;
     public Canvas canvasToTurnOff; //Need to turn off the world map canvas before going into subareas
     public EnemySpawner enemySpawner; //Sometimes we'll need to have boss battles inside subareas, we need to turn the boolean on
@@ -17,19 +18,19 @@ public class LoadScene : MonoBehaviour
 		
 		if (col.CompareTag("Player"))
 		{
-            canvasToTurnOff.gameObject.SetActive(false);
-            SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Additive);
-
             if (SceneManager.GetActiveScene().name == "PlayTest2" && unload == false)
 			{
 				Debug.Log("Disabled player");
 				player = col.gameObject;
+				player.gameObject.transform.position = new Vector3(moveOut.x, moveOut.y, player.gameObject.transform.position.z);
 				player.SetActive(false);
+				canvasToTurnOff.gameObject.SetActive(false);
+				SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Additive);
 			}
 
 			if (unload == true)
 			{
-				SceneManager.UnloadSceneAsync(unloadScene);
+				SceneManager.UnloadSceneAsync(sceneToUnLoad);
 				player.SetActive(true);
 			}
 		}
