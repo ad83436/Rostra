2409436e2886db3af0524232ba1c9c7b5b5d1f90
Dylan Tuck <45218496+Keli2k@users.noreply.github.com>
2019-11-11 02:34:21 -0500

    Updated WM enemies and Overview map

---
+++ Project-Rostra/Project Rostra/Assets/Scripts/WorldMap/NewWMEnemy.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//oo oo ah ah
+public class NewWMEnemy : MonoBehaviour
+{
+    public float speed;
+    public float rSpeed;
+    public float radius;
+
+    public Transform enemy;
+    private Transform target;
+    public GameObject player;
+
+    private bool isPatrolling = false;
+    private bool isRotatingL = false;
+    private bool isRotatingR = false;
+    private bool isMoving = false;
+    private bool isChasing = false;
+
+    void Start()
+    {
+        target = player.GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        if (isPatrolling == false && isChasing == false)
+        {
+            StartCoroutine(Patrol());
+        }
+
+        if ((target.position - enemy.position).magnitude <= radius)
+        {
+            Chase();
+        }
+
+       if(isRotatingR == true)
+        {
+            transform.Rotate(0, 0, rSpeed * Time.deltaTime);
+        }
+
+        if (isRotatingL == true)
+        {
+            transform.Rotate(0, 0, -rSpeed *Time.deltaTime);
+        }
+
+        if (isMoving == true)
+        {
+            transform.position += (transform.up * speed * Time.deltaTime);
+        }
+
+    }
+
+    IEnumerator Patrol()
+    {
+        //Random between set numbers, ex: rTime picks a random number between 1 and 2
+        int rTime = Random.Range(1, 2); 
+        int rDelay = Random.Range(1, 4);
+        int rotateLR = Random.Range(1, 2);
+        int moveDelay = Random.Range(1, 4);
+        int moveTime = Random.Range(1, 5);
+
+        isPatrolling = true;
+
+        yield return new WaitForSeconds(moveDelay);
+        isMoving = true;
+        yield return new WaitForSeconds(moveTime);
+        isMoving = false;
+        yield return new WaitForSeconds(rDelay);
+
+        if (rotateLR == 1)
+        {
+            isRotatingR = true;
+            yield return new WaitForSeconds(rTime);
+            isRotatingR = false;
+        }
+
+        if (rotateLR == 2)
+        {
+            isRotatingL = true;
+            yield return new WaitForSeconds(rTime);
+            isRotatingL = false;
+        }
+
+        isPatrolling = false;
+    }
+
+    private void Chase()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        isChasing = true;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(enemy.position, radius);
+    }
+
+}
