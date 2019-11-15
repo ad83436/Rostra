ffd2c09c2b4fea50886bd5355375977187204fdd
SeanFowke <seanfowke@hotmail.com>
2019-11-15 02:02:57 -0500

    commit

--- Project-Rostra/Project Rostra/Assets/FlickeringLight.cs
+++ Project-Rostra/Project Rostra/Assets/FlickeringLight.cs
@@ -7,7 +7,8 @@ public class FlickeringLight : MonoBehaviour
     private new Light light;
     private float lightIntensity1;
     private float lightIntensity2;
-    private float timeToChangeFlicker = 0.3f;
+	private bool lightUp = true;
+    private float timeToChangeFlicker = 0.2f;
     void Start()
     {
         light = gameObject.GetComponent<Light>();
@@ -20,15 +21,38 @@ public class FlickeringLight : MonoBehaviour
     private IEnumerator Flicker()
     {
         yield return new WaitForSeconds(timeToChangeFlicker);
-        if(light.intensity == lightIntensity1)
-        {
-            light.intensity = lightIntensity2;
-            StartCoroutine(Flicker());
-        }
-        else
-        {
-            light.intensity = lightIntensity1;
-            StartCoroutine(Flicker());
-        }
-    }
+		//if (light.intensity == lightIntensity1)
+		//{
+		//	light.intensity += 0.1f; // chanege this back to value
+		//	StartCoroutine(Flicker());
+		//}
+		//else
+		//{
+		//	light.intensity = lightIntensity1;
+		//	StartCoroutine(Flicker());
+		//}
+
+		if (light.intensity <= lightIntensity1 && lightUp == true)
+		{
+			Debug.Log("LightUp");
+			light.intensity += 0.2f;
+			StartCoroutine(Flicker());
+			if (light.intensity >= lightIntensity2)
+			{
+				lightUp = false;
+			}
+		}
+		else if(light.intensity >= lightIntensity2 && lightUp == false)
+		{
+			Debug.Log("Lightdown");
+			light.intensity -= 0.2f;
+			if (light.intensity <= lightIntensity2)
+			{
+				lightUp = true;
+			}
+			StartCoroutine(Flicker());
+		}
+
+
+	}
 }
