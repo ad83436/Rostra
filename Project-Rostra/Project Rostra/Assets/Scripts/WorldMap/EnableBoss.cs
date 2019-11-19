1cf0c58c698dad9952ba85f6d23c865d5bca201e
SeanFowke <seanfowke@hotmail.com>
2019-11-19 01:17:28 -0500

    returned g and pushing a theoretical fix

--- Project-Rostra/Project Rostra/Assets/Scripts/WorldMap/EnableBoss.cs
+++ Project-Rostra/Project Rostra/Assets/Scripts/WorldMap/EnableBoss.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class EnableBoss : MonoBehaviour
 {
 	public bool enableBoss;
-	private void OnTriggerEnter2D(Collider2D col)
+	private void OnTriggerStay2D(Collider2D col)
 	{
 		if (col.CompareTag("Player") && enableBoss == true)
 		{
+			Debug.Log("Is boss is true");
 			EnemySpawner.instance.isBoss = true;
 		}
 		if (col.CompareTag("Player") && enableBoss == false)
 		{
+			Debug.Log("Is boss is false");
 			EnemySpawner.instance.isBoss = false;
 		}
 	}
